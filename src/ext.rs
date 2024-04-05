31e023000471935a812ad24042e6135bf74cc56d
Kevin Valerio <kevin@srlabs.de>
2024-04-05 19:39:49 +0200

    Code fails on `ContractNotFound`

---
+++ src/ext.rs
@@ -0,0 +1,72 @@
+use crate::mocks::System;
+use crate::mocks::{ExistentialDeposit, Runtime, EXISTENTIAL_DEPOSIT};
+use crate::CodeHash;
+use env_logger::{Builder, Env};
+use frame::deps::{frame_system, sp_io};
+use frame::prelude::StorageVersion;
+use frame::testing_prelude::BuildStorage;
+use frame::traits::OnGenesis;
+use pallet_contracts::Pallet;
+use sp_keystore::testing::MemoryKeystore;
+use sp_keystore::KeystoreExt;
+
+pub struct ExtBuilder {
+    existential_deposit: u64,
+    storage_version: Option<StorageVersion>,
+    code_hashes: Vec<CodeHash<Runtime>>,
+}
+
+impl Default for ExtBuilder {
+    fn default() -> Self {
+        Self {
+            existential_deposit: ExistentialDeposit::get() as u64,
+            storage_version: None,
+            code_hashes: vec![],
+        }
+    }
+}
+
+impl ExtBuilder {
+    pub fn existential_deposit(mut self, existential_deposit: u64) -> Self {
+        self.existential_deposit = existential_deposit;
+        self
+    }
+    pub fn with_code_hashes(mut self, code_hashes: Vec<CodeHash<Runtime>>) -> Self {
+        self.code_hashes = code_hashes;
+        self
+    }
+    pub fn set_associated_consts(&self) {
+        EXISTENTIAL_DEPOSIT.with(|v| *v.borrow_mut() = self.existential_deposit);
+    }
+    pub fn set_storage_version(mut self, version: u16) -> Self {
+        self.storage_version = Some(StorageVersion::new(version));
+        self
+    }
+    pub fn build(self) -> sp_io::TestExternalities {
+        let env = Env::new().default_filter_or("runtime=debug");
+        let _ = Builder::from_env(env).is_test(true).try_init();
+        self.set_associated_consts();
+        let mut t = frame_system::GenesisConfig::<Runtime>::default()
+            .build_storage()
+            .unwrap();
+        pallet_balances::GenesisConfig::<Runtime> { balances: vec![] }
+            .assimilate_storage(&mut t)
+            .unwrap();
+        let mut ext = sp_io::TestExternalities::new(t);
+        ext.register_extension(KeystoreExt::new(MemoryKeystore::new()));
+        ext.execute_with(|| {
+            Pallet::<Runtime>::on_genesis();
+            if let Some(storage_version) = self.storage_version {
+                storage_version.put::<Pallet<Runtime>>();
+            }
+            System::set_block_number(1)
+        });
+        ext.execute_with(|| {
+            for code_hash in self.code_hashes {
+                panic!("TODO: fix me if you use code_hashes")
+                // CodeInfoOf::<Test>::insert(code_hash, crate::CodeInfo::new(ALICE));
+            }
+        });
+        ext
+    }
+}
--- src/mocks.rs
+++ src/mocks.rs
@@ -1,5 +1,4 @@
 use frame::deps::{frame_support, frame_system, sp_runtime};
-use frame::runtime::testing_prelude::RuntimeVersion;
 use frame::traits::IdentityLookup;
 
 use sp_runtime::{
@@ -18,9 +17,6 @@ pub type Signature = MultiSignature;
 /// to the public key of our transaction signing scheme.
 pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
 
-/// The type for looking up accounts. We don't expect more than 4 billion of them.
-pub type AccountIndex = u32;
-
 /// Balance of an account.
 pub type Balance = u128;
 
@@ -34,7 +30,6 @@ pub type Nonce = u32;
 pub type Hash = frame::deps::sp_core::H256;
 
 /// Digest item type.
-pub type DigestItem = generic::DigestItem;
 /// Header type.
 pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
 
@@ -52,33 +47,15 @@ pub type SignedExtra = (
 pub type Address = sp_runtime::MultiAddress<AccountId, ()>;
 
 pub type UncheckedExtrinsic =
-generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
+    generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
 
 pub type Block = generic::Block<Header, UncheckedExtrinsic>;
 
 type Migrations = ();
 
-/// Block ID.
-pub type BlockId = generic::BlockId<Block>;
-
-pub type RuntimeExecutive = frame::deps::frame_executive::Executive<
-    Runtime,
-    Block,
-    frame_system::ChainContext<Runtime>,
-    Runtime,
-    AllPalletsWithSystem,
-    Migrations,
->;
-
-/*
-pub type RuntimeExecutive =
-    frame::runtime::testing_prelude::Executive<Runtime, Block, frame_system::ChainContext<Runtime>, Runtime, AllPalletsWithSystem>;
-*/
-
 use frame::arithmetic::Perbill;
-use frame::deps::sp_runtime::{create_runtime_str, traits::Bounded, FixedPointNumber, Perquintill};
+use frame::deps::sp_runtime::{traits::Bounded, FixedPointNumber, Perquintill};
 use frame::deps::sp_std::prelude::*;
-use frame::deps::sp_version::NativeVersion;
 use sp_core::ConstBool;
 
 use frame_support::{
@@ -97,53 +74,12 @@ pub const CENTS: Balance = 1_000 * MILLICENTS;
 pub const DOLLARS: Balance = 100 * CENTS;
 
 pub const MILLISECS_PER_BLOCK: Moment = 3000;
-pub const SECS_PER_BLOCK: Moment = MILLISECS_PER_BLOCK / 1000;
 
 // NOTE: Currently it is not possible to change the slot duration after the chain has started.
 //       Attempting to do so will brick block production.
 pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
 
 // 1 in 4 blocks (on average, not counting collisions) will be primary BABE blocks.
-pub const PRIMARY_PROBABILITY: (u64, u64) = (1, 4);
-
-// NOTE: Currently it is not possible to change the epoch duration after the chain has started.
-//       Attempting to do so will brick block production.
-pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 10 * MINUTES;
-pub const EPOCH_DURATION_IN_SLOTS: u64 = {
-    const SLOT_FILL_RATE: f64 = MILLISECS_PER_BLOCK as f64 / SLOT_DURATION as f64;
-
-    (EPOCH_DURATION_IN_BLOCKS as f64 * SLOT_FILL_RATE) as u64
-};
-
-// These time units are defined in number of blocks.
-pub const MINUTES: BlockNumber = 60 / (SECS_PER_BLOCK as BlockNumber);
-pub const HOURS: BlockNumber = MINUTES * 60;
-pub const DAYS: BlockNumber = HOURS * 24;
-
-/// Runtime version.
-#[frame::deps::sp_version::runtime_version]
-pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("node"),
-    impl_name: create_runtime_str!("substrate-node"),
-    authoring_version: 10,
-    // Per convention: if the runtime behavior changes, increment spec_version
-    // and set impl_version to 0. If only runtime
-    // implementation changes and behavior does not, then leave spec_version as
-    // is and increment impl_version.
-    spec_version: 268,
-    impl_version: 0,
-    apis: frame::deps::sp_version::create_apis_vec!([]),
-    transaction_version: 2,
-    state_version: 1,
-};
-
-/// Native version.
-pub fn native_version() -> NativeVersion {
-    NativeVersion {
-        runtime_version: VERSION,
-        can_author_with: Default::default(),
-    }
-}
 
 parameter_types! {
 pub const BlockHashCount: BlockNumber = 100;
@@ -275,6 +211,7 @@ impl pallet_contracts::Config for Runtime {
     type Randomness = Randomness;
     type Currency = Balances;
     type RuntimeEvent = RuntimeEvent;
+    type Migrations = ();
     type RuntimeCall = RuntimeCall;
     /// The safest default is to allow no calls at all.
     ///
@@ -283,26 +220,25 @@ impl pallet_contracts::Config for Runtime {
     /// change because that would break already deployed contracts. The `Call` structure itself
     /// is not allowed to change the indices of existing pallets, too.
     type CallFilter = frame_support::traits::Nothing;
-    type DepositPerItem = DepositPerItem;
-    type DepositPerByte = DepositPerByte;
-    type DefaultDepositLimit = DefaultDepositLimit;
-    type CallStack = [pallet_contracts::Frame<Self>; 5];
     type WeightPrice = pallet_transaction_payment::Pallet<Self>;
     type WeightInfo = pallet_contracts::weights::SubstrateWeight<Self>;
     // type ChainExtension = LocalChainExtensions<Self, UnifiedAccounts, Xvm>;
     type ChainExtension = ();
     type Schedule = Schedule;
+    type CallStack = [pallet_contracts::Frame<Self>; 5];
+    type DepositPerByte = DepositPerByte;
+    type DefaultDepositLimit = DefaultDepositLimit;
+    type DepositPerItem = DepositPerItem;
+    type CodeHashLockupDepositPercent = CodeHashLockupDepositPercent;
     type AddressGenerator = pallet_contracts::DefaultAddressGenerator;
     type MaxCodeLen = ConstU32<{ 123 * 1024 }>;
     type MaxStorageKeyLen = ConstU32<128>;
+    type MaxDelegateDependencies = MaxDelegateDependencies;
     type UnsafeUnstableInterface = ConstBool<true>;
     type MaxDebugBufferLen = ConstU32<{ 2 * 1024 * 1024 }>;
-    type MaxDelegateDependencies = MaxDelegateDependencies;
-    type CodeHashLockupDepositPercent = CodeHashLockupDepositPercent;
     type RuntimeHoldReason = RuntimeHoldReason;
     type Debug = ();
     type Environment = ();
-    type Migrations = ();
     type ApiVersion = ();
     type Xcm = ();
 }
